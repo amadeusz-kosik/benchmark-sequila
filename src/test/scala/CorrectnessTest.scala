3c25d2815915fd0bb7775cb1b5e43c3f9ca1a18a
Amadeusz Kosik <tappers_teams.5d@icloud.com>
2026-05-05 17:53:26 +0200

    Add unit tests for benchmark.

---
+++ src/test/scala/CorrectnessTest.scala
@@ -0,0 +1,60 @@
+import benchmark.sequila.Benchmark
+import com.holdenkarau.spark.testing.DataFrameSuiteBase
+import org.apache.spark.sql.SparkSession
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
+
+
+class CorrectnessTest extends AnyFunSpec with Matchers with DataFrameSuiteBase {
+
+    describe("Sequila benchmark") {
+
+      it("should return empty list if no intervals overlap") {
+        implicit val sparkSession: SparkSession = spark
+
+        val database = TestDataGenerator.consecutive(100)
+        val query    = TestDataGenerator.consecutive(100, 200)
+
+        val expected = 0L
+        val actual = Benchmark.runBenchmark(database, query).result
+
+        actual should equal (expected)
+      }
+
+      it("should correctly map 1:1 relation") {
+        implicit val sparkSession: SparkSession = spark
+
+        val database = TestDataGenerator.consecutive(100)
+        val query    = TestDataGenerator.consecutive(100)
+
+        val expected = 100L
+        val actual = Benchmark.runBenchmark(database, query).result
+
+        actual should equal (expected)
+      }
+
+      it("should correctly map 1:all relation") {
+        implicit val sparkSession: SparkSession = spark
+
+        val database = TestDataGenerator.consecutive(1, 0, 100)
+        val query    = TestDataGenerator.consecutive(100)
+
+        val expected = 100L
+        val actual = Benchmark.runBenchmark(database, query).result
+
+        actual should equal (expected)
+      }
+
+      it("should correctly map all:1 relation") {
+        implicit val sparkSession: SparkSession = spark
+
+        val database = TestDataGenerator.consecutive(100)
+        val query    = TestDataGenerator.consecutive(1, 0, 100)
+
+        val expected = 100L
+        val actual = Benchmark.runBenchmark(database, query).result
+
+        actual should equal (expected)
+      }
+    }
+}
---
+++ src/test/scala/TestDataGenerator.scala
@@ -0,0 +1,27 @@
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.{functions => F}
+
+
+object TestDataGenerator {
+
+  def consecutive(rowsCount: Int)(implicit spark: SparkSession): DataFrame =
+    generate(rowsCount, 0, 0)
+
+  def consecutive(rowsCount: Int, offset: Int)(implicit spark: SparkSession): DataFrame =
+    generate(rowsCount, offset, 0)
+
+  def consecutive(rowsCount: Int, offset: Int, rowWidth: Int)(implicit spark: SparkSession): DataFrame =
+    generate(rowsCount, offset, rowWidth)
+
+
+  private def generate(rowsCount: Int, offset: Int, rowWidth: Int)(implicit spark: SparkSession): DataFrame = {
+    spark
+      .range(0, rowsCount)
+      .toDF("i")
+      .select(
+        (F.col("i") + F.lit(offset)).as("from"),
+        (F.col("i") + F.lit(offset) + F.lit(rowWidth)).as("to")
+      )
+      .withColumn("key", F.lit("no_grp"))
+  }
+}
